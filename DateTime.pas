f57e3f161a22de6a29afe43433ee272aff9aa68a
local user <user@mil8a-101-11.mmcs.sfedu.ru>
2024-10-05 14:17:49 +0300

    Add convert from minutes to second

---
+++ DateTime.pas
@@ -0,0 +1,13 @@
+﻿begin
+  var year := ReadInteger('Введите целый положительный номер года: ');
+  Assert(year > 0);
+  var rez: boolean;
+  if year.Divs(4) and not year.Divs(100) then
+    rez := True
+  else if year.Divs(400) then
+    rez := True
+  else
+    rez := False;
+  Println($'Год високосный: {rez}');
+  Println($'В минуте {60} секунд');
+end.
